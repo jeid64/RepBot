44bf81eb728473b99a7c5bec950681c6654a1e91
Julien Eid <jeid@csh.rit.edu>
2013-07-10 21:09:36 -0400

    LOL TYPES.

--- RepBot.py
+++ RepBot.py
@@ -154,10 +154,10 @@ class PrePostfixRepChange(RepChangeCommand):
             self.setValid(True)
         elif len(self.getUser().split()) == 2:
             if self.getUser().split()[0].isdigit():
-                self.multiplier = self.getUser().split()[0]
+                self.multiplier = int(self.getUser().split()[0])
                 self.setValid(True)
             elif self.getUser().split()[1].isdigit():
-                self.multiplier = self.getUser().split()[1]
+                self.multiplier = int(self.getUser().split()[1])
                 self.setValid(True)
 
     def perform(self, val):
